3cecaef322d9c3b3e3039c0005f972a8c3be4dd8
patricksilva1 <patricksilva5160@gmail.com>
2022-12-23 15:47:27 -0300

    handler

--- app-users-api/src/main/java/dev/patricksilva/users/GetUserHandler.java
+++ app-users-api/src/main/java/dev/patricksilva/users/GetUserHandler.java
@@ -1,14 +1,32 @@
 package dev.patricksilva.users;
 
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.JsonObject;
 
 public class GetUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
-        return null;
+
+        Map<String, String> pathParameters = input.getPathParameters();
+
+        String userId = pathParameters.get("userId");
+
+        JsonObject returnValue = new JsonObject();
+
+        returnValue.addProperty("firstName", "Patrick");
+        returnValue.addProperty("lastName", "Loureiro");
+        returnValue.addProperty("id", userId);
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+
+        response.withStatusCode(200).withBody(returnValue.toString());
+
+        return response;
     }
 }
